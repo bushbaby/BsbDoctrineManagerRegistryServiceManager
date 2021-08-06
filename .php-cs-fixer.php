cdb285a1647b6fbb684c9934d2a740eacd3f8347
Bas Kamer <bas@bushbaby.nl>
2021-08-06 23:06:37 +0200

    cs tooling

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -9,9 +9,9 @@
 declare(strict_types=1);
 
 $config = new Bsb\CS\Config();
-$config->getFinder()->in(__DIR__)->append(['.php_cs']);
+$config->getFinder()->in(__DIR__)->append(['.php-cs-fixer.php']);
 
-$cacheDir = \getenv('TRAVIS') ? \getenv('HOME') . '/.php-cs-fixer' : __DIR__;
+$cacheDir = getenv('TRAVIS') ? getenv('HOME') . '/.php-cs-fixer' : __DIR__;
 
 $config->setCacheFile($cacheDir . '/.php_cs.cache');
 
